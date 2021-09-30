4239ef8908af7b9ad5e6bd031045ed7947647f8f
sydney <sydneychase5841@gmail.com>
2021-09-30 12:22:21 -0400

    testing what shows for blogger-index

--- src/Components/User.js
+++ src/Components/User.js
@@ -5,10 +5,10 @@ function User({user}) {
 
     return (
         <div className='user-card'>
-            <p> {user.first_name} </p>
-            <p> {user.last_name} </p>
-            <p> {user.user_name} </p>
-            <p> {user.email} </p>
+            <p> Blogger First Name: {user.first_name} {user.last_name} </p>
+            {/* <p> {user.last_name} </p> */}
+            <p> User Name: {user.user_name} </p>
+            {/* <p> Blogger Email address:  {user.email} </p> */}
             
         </div>
     );
