d04dfce5747fde6bee3fb46834433e11b093dd27
sydney <sydneychase5841@gmail.com>
2021-10-01 00:23:30 -0400

    updated user form to add required input fields

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -22,13 +22,13 @@ const handleSubmit = (e) => {
             
             <form onSubmit={handleSubmit} className="user-form">
                 <label className="form-label">First Name</label>
-                <input type="text" placeholder='Type First Name Here' name="first_name" value={user.first_name} onChange={handleChange} />
+                <input type="text" placeholder='Type First Name Here' required name="first_name" value={user.first_name} onChange={handleChange} />
                 <label className="form-label">Last Name</label>
-                <input type="text" placeholder='Type Last Name Here' name="last_name" value={user.last_name} onChange={handleChange} />
+                <input type="text" placeholder='Type Last Name Here' required name="last_name" value={user.last_name} onChange={handleChange} />
                 <label className="form-label">User Name</label>
-                <input type="text" placeholder='Create a User Name' name="user_name" value={user.user_name} onChange={handleChange} />
+                <input type="text" placeholder='Create a User Name' required name="user_name" value={user.user_name} onChange={handleChange} />
                 <label className="form-label">Email</label>
-                <input type="text" placeholder='Type your email address' name="email" value={user.email} onChange={handleChange} />
+                <input type="text" placeholder='Type your email address' required name="email" value={user.email} onChange={handleChange} />
                 <button type="submit">Create My Account</button>
             </form>
             <p>Or a reader by creating your account below. You don't have to blog to register. You can read blogs, post a blog or comment on one.</p>
