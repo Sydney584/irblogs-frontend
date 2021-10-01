b66c5dd717e33ecc734ec90a5baa8073453370cb
sydney <sydneychase5841@gmail.com>
2021-09-30 23:25:31 -0400

    added userForm component to UserContainer

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+
 function User({user}) {
 
 
     return (
-        <div className='user-card'>
+        <div className='user-card' key={user.id}>
             <p> Blogger First Name: {user.first_name} {user.last_name} </p>
             {/* <p> {user.last_name} </p> */}
             <p> User Name: {user.user_name} </p>
+            {/* <p>User Blogpost: {user.blogposts}</p> */}
             {/* <p> Blogger Email address:  {user.email} </p> */}
             
+            <div className="userform">
+    
+            </div>
         </div>
     );
 }
